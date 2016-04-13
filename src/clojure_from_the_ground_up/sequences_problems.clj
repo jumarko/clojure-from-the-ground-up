683fd5a6510da4591b20d4b28f1bc26fb66e9fce
Juraj Martinka <juraj@snapshot.travel>
2016-04-13 10:06:15 +0200

    Complete additional problems for sequnces

--- src/clojure_from_the_ground_up/sequences_problems.clj
+++ src/clojure_from_the_ground_up/sequences_problems.clj
@@ -19,3 +19,69 @@
       ;; when there's odd number of characters we need to ignore the first character from second half
       (equal-in-reverse-order (vector (first halves) ( rest ( last halves)))))
     ))
+
+
+
+;;;
+;;; Find the number of c's in "abracadabra".
+;;;
+(defn number-of-c
+  "Returns number of c's in the given string."
+  [s]
+  (if (empty? s)
+    0
+    (get (frequencies s) \c)))
+
+
+
+;;;
+;;; Write your own version of filter
+;;;
+
+(defn my-filter [pred coll]
+  (if (not (seq coll))
+    coll
+    (loop [head (first coll)
+           tail (rest coll)]
+      (if (pred head)
+        (cons head (my-filter pred tail))
+        (my-filter pred tail)))))
+
+(my-filter (fn [x] true) [1 2 3])
+(my-filter odd? [1 2 3])
+
+
+
+;;;
+;;; Find the first 100 prime numbers
+;;;
+
+(defn- divisors
+  "Checking all the candidates returns only those that are real divisors of x."
+  [x candidates]
+  (filter (fn [candidate] (zero? (mod x candidate)))
+          candidates))
+
+(divisors 10 [2 3 4 5]) ;=> (2, 5)
+
+(defn- is-prime? [x]
+  (cond
+   (< x 2) false
+   (< x 4) true
+   :else (let [x-root (Math/sqrt x)
+               number-of-divisors (count (divisors x (range 2 (inc x-root))))]
+           (zero? number-of-divisors))))
+
+(map is-prime? [1 2 3 4 5 6 7 8 9 10 11])
+
+(defn- all-prime-numbers
+  "Generates infinite sequence of prime numbers."
+  []
+  (filter is-prime? (range)))
+
+(defn prime-numbers
+  "Returns first n prime numbers."
+   [n]
+   (take n (all-prime-numbers)))
+
+(prime-numbers 10)
--- test/clojure_from_the_ground_up/sequences_problems_test.clj
+++ test/clojure_from_the_ground_up/sequences_problems_test.clj
@@ -16,3 +16,25 @@
        "none"
        "motor"
        "tongue"))
+
+
+
+(deftest check-number-of-c
+  (is (= 1 (number-of-c "abracadabra"))))
+
+
+
+(deftest filter-always-true
+  (let [true-fn (fn [x] true)]
+    (is (empty? (my-filter true-fn [])))
+    (is (= [1 2 3] (my-filter true-fn [1 2 3])))))
+
+(deftest filter-even-numbers
+  (is ( =
+        [0 2 4 6]
+        (my-filter even? [0 1 2 3 4 5 6 7]))))
+
+
+
+(deftest get-prime-numbers
+  (is (= [2 3 5 7 11 13 17] (prime-numbers 7))))
